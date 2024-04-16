4470d327f67a1ced3af87079e7ac9414f7ac389b
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-04-16 13:18:53 +0700

    Implement subscribe function in Notification controller

---
+++ src/controller/notification.rs
@@ -0,0 +1,8 @@
+#[post("/subscribe/<product_type>", data= "<subscriber>")]
+pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
+    return match NotificationService::subscribe(product_type, subscriber.into_inner()) {
+        Ok(f) => Ok(Created::new("/").body(Json::from(f)),
+        Err(e) => Err(e)
+    };
+
+}
