bdde93eef2da8c53b22bced82fb2bdfa9f920551
gregoriussamuelh@gmail.com <gregoriussamuelh@gmail.com>
2024-04-16 14:54:13 +0700

    Commit reflection publisher-3 in README.md

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -30,12 +30,12 @@ impl NotificationService {
         return Ok(result.unwrap());
 
     }
-    pub fn notify(&self, product_Type: &str, status: &str, product: Product) {
+    pub fn notify(&self, product_type: &str, status: &str, product: Product) {
         let mut payload: Notification = Notification {
             product_title: product.clone().title,
             product_type: String::from(product_type),
             product_url: product.clone().get_url(),
-            subscriber_name: String::;from(""),
+            subscriber_name: String::from(""),
             status: String::from(status)
 
         };
